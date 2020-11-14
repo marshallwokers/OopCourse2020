ff5ee499c47859cc7a42bb715be52d9528dc779e
marshallwokers <marshallwokers@gmail.com>
2020-11-14 14:28:40 +0700

    Range*: реализованы методы пересечения (getIntersection), объединения (getUnion) и разности (getDifference) интервалов. В main добавлен код для проверки этих методов.

--- Range/src/ru/academit/babynina/ProgramForRange.java
+++ Range/src/ru/academit/babynina/ProgramForRange.java
@@ -6,26 +6,45 @@ public class ProgramForRange {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите начальное число диапазона: ");
-        double from = scanner.nextDouble();
+        System.out.print("Введите начальное число первого диапазона: ");
+        double firstRangeFrom = scanner.nextDouble();
 
-        System.out.print("Введите конечное число диапазона: ");
-        double to = scanner.nextDouble();
+        System.out.print("Введите конечное число первого диапазона: ");
+        double firstRangeTo = scanner.nextDouble();
 
-        System.out.print("Введите число для проверки: ");
-        double number = scanner.nextDouble();
+        System.out.print("Введите начальное число второго диапазона: ");
+        double secondRangeFrom = scanner.nextDouble();
 
-        Range userRange = new Range(from, to);
+        System.out.print("Введите конечное число второго диапазона: ");
+        double secondRangeTo = scanner.nextDouble();
 
-        System.out.println("Длина диапазона равна: " + userRange.getLength());
+        Range firstRange = new Range(firstRangeFrom, firstRangeTo);
+        Range secondRange = new Range(secondRangeFrom, secondRangeTo);
 
-        if (userRange.isInside(number)) {
-            System.out.println("Введенное число принадлежит диапазону");
+        Range intersectionRangeResult = firstRange.getIntersection(secondRange);
+
+        if (intersectionRangeResult == null) {
+            System.out.println("Интервалы не имеют пересечений.");
         } else {
-            System.out.println("Введенное число не принадлежит диапазону");
+            System.out.println("Интервал-пересечение двух интервалов равен: от " + intersectionRangeResult.getFrom() + " до " + intersectionRangeResult.getTo());
         }
-        userRange.setFrom(from + 1);
-        userRange.setTo(to + 1);
-        System.out.println("Диапазон, смещенный на единицу от " + userRange.getFrom() + " до " + userRange.getTo());
+
+        Range[] unionRangeResult = firstRange.getUnion(secondRange);
+
+        if (unionRangeResult.length == 1) {
+            System.out.println("Интервал-объединение двух интервалов равен: от " + unionRangeResult[0].getFrom() + " до " + unionRangeResult[0].getTo());
+        } else {
+            System.out.println("Интервалы-объединение двух интервалов равен: от " + unionRangeResult[0].getFrom() + " до " + unionRangeResult[0].getTo() + " и от " + unionRangeResult[1].getFrom() + " до " + unionRangeResult[1].getTo());
+        }
+
+        Range[] differenceRangeResult = firstRange.getDifference(secondRange);
+
+        if (differenceRangeResult == null) {
+            System.out.println("Интервал-разность не найден, т.к. интервалы не имеют пересечений");
+        } else if (differenceRangeResult.length == 1) {
+            System.out.println("Интервал-разность двух интервалов равен: от " + differenceRangeResult[0].getFrom() + " до " + differenceRangeResult[0].getTo());
+        } else {
+            System.out.println("Интервалы-разность двух интервалов равен: от " + differenceRangeResult[0].getFrom() + " до " + differenceRangeResult[0].getTo() + " и от " + differenceRangeResult[1].getFrom() + " до " + differenceRangeResult[1].getTo());
+        }
     }
 }
--- Range/src/ru/academit/babynina/Range.java
+++ Range/src/ru/academit/babynina/Range.java
@@ -32,4 +32,145 @@ public class Range {
     public boolean isInside(double number) {
         return number >= from && number <= to;
     }
+
+    public Range getIntersection(Range range) {
+        double resultRangeFrom;
+        double resultRangeTo;
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+
+        if (this.from == rangeFrom) {
+            resultRangeFrom = this.from;
+        } else if (this.from < rangeFrom) {
+            if (this.isInside(rangeFrom)) {
+                resultRangeFrom = rangeFrom;
+            } else {
+                return null;
+            }
+        } else {
+            if (range.isInside(this.from)) {
+                resultRangeFrom = this.from;
+            } else {
+                return null;
+            }
+        }
+
+        if (this.to == rangeTo) {
+            resultRangeTo = this.to;
+        } else if (this.to < rangeTo) {
+            resultRangeTo = this.to;
+        } else {
+            resultRangeTo = range.to;
+        }
+
+        if (resultRangeFrom == resultRangeTo) {
+            return null;
+        } else {
+            return new Range(resultRangeFrom, resultRangeTo);
+        }
+    }
+
+    public Range[] getUnion(Range range) {
+        double firstResultRangeFrom;
+        double firstResultRangeTo;
+        double secondResultRangeFrom = 0;
+        double secondResultRangeTo = 0;
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+
+        if (this.from == rangeFrom) {
+            firstResultRangeFrom = this.from;
+        } else if (this.from < rangeFrom) {
+            if (this.isInside(rangeFrom)) {
+                firstResultRangeFrom = this.from;
+            } else {
+                firstResultRangeFrom = this.from;
+                secondResultRangeFrom = rangeFrom;
+            }
+        } else {
+            if (range.isInside(this.from)) {
+                firstResultRangeFrom = rangeFrom;
+            } else {
+                firstResultRangeFrom = rangeFrom;
+                secondResultRangeFrom = this.from;
+            }
+        }
+
+        if (this.to == rangeTo) {
+            firstResultRangeTo = this.to;
+        } else if (this.to < rangeTo) {
+            if (range.isInside(this.to)) {
+                firstResultRangeTo = rangeTo;
+            } else {
+                firstResultRangeTo = this.to;
+                secondResultRangeTo = rangeTo;
+            }
+        } else {
+            firstResultRangeTo = this.to;
+        }
+
+        Range[] resultRanges;
+        Range firstResultRange = new Range(firstResultRangeFrom, firstResultRangeTo);
+
+        if (secondResultRangeFrom == 0 && secondResultRangeTo == 0) {
+            resultRanges = new Range[1];
+            resultRanges[0] = firstResultRange;
+        } else {
+            Range secondResultRange = new Range(secondResultRangeFrom, secondResultRangeTo);
+            resultRanges = new Range[2];
+            resultRanges[0] = firstResultRange;
+            resultRanges[1] = secondResultRange;
+        }
+        return resultRanges;
+    }
+
+    public Range[] getDifference(Range range) {
+        double firstResultRangeFrom;
+        double firstResultRangeTo;
+        double secondResultRangeFrom = 0;
+        double secondResultRangeTo = 0;
+        double rangeFrom = range.getFrom();
+        double rangeTo = range.getTo();
+
+        if (this.isInside(rangeFrom)) {
+            if (this.to == rangeFrom) {
+                return null;
+            } else {
+                firstResultRangeFrom = this.from;
+
+                if (this.isInside(rangeTo)) {
+                    firstResultRangeTo = rangeFrom;
+                    secondResultRangeFrom = rangeTo;
+                    secondResultRangeTo = this.to;
+                } else {
+                    firstResultRangeTo = rangeFrom;
+                }
+            }
+        } else {
+            if (this.isInside(rangeTo)) {
+                if (rangeTo == this.from) {
+                    return null;
+                } else {
+                    firstResultRangeFrom = rangeTo;
+                    firstResultRangeTo = this.to;
+                }
+            } else {
+                return null;
+            }
+        }
+
+        Range[] resultRanges;
+        Range firstResultRange = new Range(firstResultRangeFrom, firstResultRangeTo);
+
+        if (secondResultRangeFrom == 0 && secondResultRangeTo == 0) {
+            resultRanges = new Range[1];
+            resultRanges[0] = firstResultRange;
+        } else {
+            Range secondResultRange = new Range(secondResultRangeFrom, secondResultRangeTo);
+            resultRanges = new Range[2];
+            resultRanges[0] = firstResultRange;
+            resultRanges[1] = secondResultRange;
+        }
+        return resultRanges;
+    }
 }
