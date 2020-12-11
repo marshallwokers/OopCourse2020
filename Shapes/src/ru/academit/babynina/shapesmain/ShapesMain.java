9a69f6d33b2cab760cb625a3086aa05486d2da31
marshallwokers <marshallwokers@gmail.com>
2020-12-11 18:22:32 +0700

    Исправлена ошибка вызова toString

--- Shapes/src/ru/academit/babynina/shapesmain/ShapesMain.java
+++ Shapes/src/ru/academit/babynina/shapesmain/ShapesMain.java
@@ -20,7 +20,7 @@ public class ShapesMain {
     public static void main(String[] args) {
         Shapes[] shapes = {new Square(5), new Triangle(-2, 2, 3, 2, -1, 0), new Rectangle(3, 7), new Circle(50), new Square(15), new Triangle(0, -1, 0, 5, 34, 0), new Rectangle(13, 17), new Circle(6)};
 
-        System.out.println("Фигура с максимальной площадью - " + getMaxAreaShape(shapes).toString());
-        System.out.println("\n\nФигура со вторым по величине периметром - " + getSecondMaxPerimeterShape(shapes).toString());
+        System.out.println("Фигура с максимальной площадью - " + getMaxAreaShape(shapes));
+        System.out.println("\n\nФигура со вторым по величине периметром - " + getSecondMaxPerimeterShape(shapes));
     }
 }
--- Vector/src/ru/academit/babynina/vectormain/VectorMain.java
+++ Vector/src/ru/academit/babynina/vectormain/VectorMain.java
@@ -9,20 +9,20 @@ public class VectorMain {
         Vector vector3 = new Vector(vector2);
         Vector vector4 = new Vector(new double[]{6.0, 5.0, 4.0, 3.0, 2.0, 1.0});
 
-        System.out.println("vector1 : " + vector1.toString());
-        System.out.println("vector2 : " + vector1.toString());
-        System.out.println("vector3 : " + vector1.toString());
-        System.out.println("vector4 : " + vector1.toString() + "\n\n");
-        System.out.println("vector2 после прибавления к нему vector4 равен : " + vector2.sum(vector4).toString());
-        System.out.println("vector1 после вычитания из него vector2 равен : " + vector1.subtract(vector2).toString());
-        System.out.println("vector2 после умножения на скаляр 3 равен : " + vector2.multiply(3).toString());
-        System.out.println("Развернутый vector1 : " + vector1.inverse().toString());
+        System.out.println("vector1 : " + vector1);
+        System.out.println("vector2 : " + vector1);
+        System.out.println("vector3 : " + vector1);
+        System.out.println("vector4 : " + vector1 + "\n\n");
+        System.out.println("vector2 после прибавления к нему vector4 равен : " + vector2.sum(vector4));
+        System.out.println("vector1 после вычитания из него vector2 равен : " + vector1.subtract(vector2));
+        System.out.println("vector2 после умножения на скаляр 3 равен : " + vector2.multiply(3));
+        System.out.println("Развернутый vector1 : " + vector1.inverse());
         System.out.println("Длина vector3 равна: " + vector3.getLength());
         vector4.setVectorComponentByIndex(0, 10.0);
         System.out.println("Компонент vector4 по индексу 0 равен: " + vector4.getVectorComponentByIndex(0));
         System.out.println("Хэш-код vector4 равен: " + vector4.hashCode());
         Vector sumResultVector = Vector.getSumResultVector(vector1, vector2);
-        System.out.println("Результирующий вектор сложения векторов vector1 и vector2 равен: " + sumResultVector.toString());
+        System.out.println("Результирующий вектор сложения векторов vector1 и vector2 равен: " + sumResultVector);
 
         if (sumResultVector.equals(vector1)) {
             System.out.println("Векторы sumResultVector и vector1 равны");
@@ -30,7 +30,7 @@ public class VectorMain {
             System.out.println("Векторы sumResultVector и vector1 не равны");
         }
 
-        System.out.println("Результирующий вектор вычитания векторов sumResultVector и vector1 равен: " + Vector.getSubtractResultVector(sumResultVector, vector1).toString());
-        System.out.println("Результирующий вектор скалярного произведения векторов sumResultVector и vector1 равен: " + Vector.getMultiplyResultVector(sumResultVector, vector3).toString());
+        System.out.println("Результирующий вектор вычитания векторов sumResultVector и vector1 равен: " + Vector.getSubtractResultVector(sumResultVector, vector1));
+        System.out.println("Результирующий вектор скалярного произведения векторов sumResultVector и vector1 равен: " + Vector.getMultiplyResultVector(sumResultVector, vector3));
     }
 }
